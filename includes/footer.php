7c8c87af649c12eb4ef3ce7ab33b7aa721323c4b
John <johnpolhill42@gmail.com>
2015-10-14 00:07:32 +0000

    test

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer>
+	<h4>My Social Media Pages</h4>
+	<ul>
+		<li><a href="https://www.facebook.com/john.polhill.3">My Facebook</a></li>
+		<li><a href="https://www.linkedin.com/profile/view?id=AAIAABqp07wByyz-f2f-dwmX4z2h9xvQtDSoj9s">My LinkedIn</a></li>
+		<li><a href="https://github.com/JohnP42">My GitHub</a></li>
+		<li><a href="https://twitter.com/JohnPolhill42">Twitter @JohnPolhill42</a></li>
+	</ul>
+</footer>
